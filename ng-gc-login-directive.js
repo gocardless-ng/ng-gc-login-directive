99844b9311b4503f097868f0c321e954d3dbf398
Philip Harrison <philip@mailharrison.com>
2013-09-15 18:44:23 +0100

    Fix build

--- ng-gc-login-directive.js
+++ ng-gc-login-directive.js
@@ -3,13 +3,17 @@
  * (c) 2013-2013 GoCardless, Ltd.
  * https://github.com/gocardless-ng/ng-gc-login-directive.git
  * License: MIT
- */angular.module('login-template.html', []).run(function($templateCache) {
+ */
+(function(){
+'use strict';
+
+angular.module('login-template.html', []).run(['$templateCache', function($templateCache) {
   $templateCache.put('login-template.html',
     '<div><dialog show="isVisible" ng-class="\'login-dialog\'" template-url="empty" options="{\n' +
     '      preventHideOnClick: true,\n' +
     '      preventHideOnEscape: true\n' +
     '    }"><form ng-submit="signIn()" name="loginForm"><div class="u-padding-Am"><div class="error-label u-margin-Bm" ng-show="signInError"><span>{{ signInError }}</span></div><label for="login-email" class="label label--stacked">Email</label><input type="email" id="login-email" name="loginEmail" ng-model="loginDetails.email" class="input input--stacked input--block"><div class="error-label" ng-show="loginForm.loginEmail.$dirty && form.loginEmail.$invalid"><span ng-show="loginForm.loginEmail.$error.required">Please provide a valid email</span></div><label for="login-password" class="label label--stacked">Password</label><input type="password" id="login-password" name="loginPassword" ng-model="loginDetails.password" class="input input--stacked input--block"><div class="error-label" ng-show="loginForm.loginPassword.$dirty && form.loginPassword.$invalid"><span ng-show="loginForm.loginPassword.$error.required">Please provide a password</span></div><input type="submit" class="btn btn--info btn--block" value="Sign in"></div></form></dialog></div>');
-});
+}]);
 
 'use strict';
 
@@ -74,3 +78,4 @@ angular.module('gc.login', [
 
   }
 ]);
+})();
